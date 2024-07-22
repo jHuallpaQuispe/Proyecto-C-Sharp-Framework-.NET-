2b2e541ad028a7a3c0478821b9a2991cc4b54d48
Jherson Huallpa Quispe <anthony741500@gmail.com>
2024-07-21 22:04:44 -0300

    Agregamos el boton de eliminacion logica, encapsulamos el codigo de eliminar

--- App-Discos-2024/frmDiscos.cs
+++ App-Discos-2024/frmDiscos.cs
@@ -86,5 +86,46 @@ namespace App_Discos_2024
             //Cuando se cierra la pantalla abierta,actualizaremos la carga
             cargar();
         }
+
+        private void btnEliminacionFisico_Click(object sender, EventArgs e)
+        {
+            eliminar();
+        }
+
+        private void btnEliminacionLogico_Click(object sender, EventArgs e)
+        {
+            eliminar(true);
+        }
+
+        private void eliminar( bool logico = false)
+        {
+            //Encapsulamos el codigo porque se repetiria en los dos evente de eliminar
+            DiscoBusiness business = new DiscoBusiness();
+            Disco seleccionado;
+
+            try
+            {
+                DialogResult resultado = MessageBox.Show("¿De verdad quieres eliminarlo?", "¡Cuidado!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (resultado == DialogResult.Yes)
+                {
+                    seleccionado = (Disco)dgvDiscos.CurrentRow.DataBoundItem;
+
+                    //Si es true estamos en una eliminacion logica y si es FALSE estamos en una eliminacion Fisica
+                    if (logico)
+                        business.eliminarLogico(seleccionado.Id);
+                    else
+                        business.eliminarFisico(seleccionado.Id);
+
+                    cargar(); // actualizamos la lista
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
     }
 }
